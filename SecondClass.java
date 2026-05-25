01141132ea5612856c7d6f5db825fca2677d8ad5
Satvik <satviktyagi3@gmail.com>
2026-05-26 03:21:40 +0530

    Second lecture

--- SecondClass.java
+++ SecondClass.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class SecondClass {
-    
+    //if_else
+    public static void main(String[] args){
+        // System.out.println("Hello!");
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+
+        if(age>18){
+            System.out.println("Adult");
+        }
+        else{System.out.println("Not Adult");}
+    }
 }
